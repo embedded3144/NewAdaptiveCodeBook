27899014beacdafe876525eff9bb72c6133c7af9
고형호 <hyungho.ko@mirero.co.kr>
2019-04-11 11:50:58 +0900

    과제 추가

---
+++ 2019년 4월/과제/SolidRating/SolidRating.Tests/RatingEngineRate_Should.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SolidRating.Tests
+{
+    public class RatingEngineRate_Should
+    {
+        [Fact]
+        public void ReturnsRatingOf10000For200000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 200000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(10000, result);
+        }
+
+        [Fact]
+        public void ReturnsRatingOf0For200000BondOn260000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 260000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
+
+            Assert.Equal(0, result);
+        }
+    }
+}
